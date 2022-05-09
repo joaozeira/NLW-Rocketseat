5f630990ae5e9c54ec9dcba4eb9d1f67059319a1
João Carlos <56844688+joaozeira@users.noreply.github.com>
2022-05-09 20:29:20 -0300

    Terceira aula do NLW - Rocketseat

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function onScroll() {
   }
 }
 
+
+// ------- MENU ------- // 
  
 function openMenu() {
   document.body.classList.add('menu-expanded')
@@ -14,3 +16,20 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+/* --- ScrollReveal Lib --- */
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .card,
+#about,
+#about Headers,
+#about .content`);
